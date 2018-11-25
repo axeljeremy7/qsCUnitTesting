c4e9ea51abc4b8fc87cdd516718c8e6aa56464b4
axeljeremy7 <axel.jeremy.7@gmail.com>
2018-11-24 22:06:06 -0500

    modifying/validate for STDIN

--- src/main.c
+++ src/main.c
@@ -1,12 +1,17 @@
 #include "main.h"
 
+// main this for stdin
+// other_file for testing
+// I think my internet crashed
+// or atleast discord crashed
+
 void init_var(nummber_parts *v);
 int memory_ok(Line *linein, FILE *fp);
 int main(int argc, char const *argv[])
 {
     FILE *fp;
     printf("PASS LINE 7\n");
-    fp = fopen("logs/user_logs.txt", "w");
+    log = fopen("logs/user_logs.txt", "w");
     char a_array[512], b_array[512], c_array[512];
     roots_numbers result;
     double d;
@@ -18,7 +23,15 @@ int main(int argc, char const *argv[])
     if (memory_ok(&linein, fp) == 1)
     {
         printf("PASS LINE 17\n");
-       
+        if (solve_equation_yes_no(&response, log) == 1)
+        {
+        }
+        else
+        {
+            free(linein.str);
+            return -1;
+        }
+
         free(linein.str);
     }
     else
@@ -38,18 +51,18 @@ int main(int argc, char const *argv[])
         result.c = c;
 
         printf("==> %s %s %s\n", a_array, b_array, c_array);
-        fprintf(fp, "\nMAIN ==> %s %s %s\n", a_array, b_array, c_array);
-        if (validation_floats(a_array, b_array, c_array, &a, &b, &c, fp) == 1)
+        fprintf(log, "\nMAIN ==> %s %s %s\n", a_array, b_array, c_array);
+        if (validation_floats(a_array, b_array, c_array, &a, &b, &c, log) == 1)
         {
-            get_precision(a_array, b_array, c_array, &a, &b, &c, fp);
-            add_numbers_to_result(&result, &a, &b, &c, fp,
+            get_precision(a_array, b_array, c_array, &a, &b, &c, log);
+            add_numbers_to_result(&result, &a, &b, &c, log,
                                   a_array, b_array, c_array);
 
             /*printf("\n\ta == %f", result.a.f_number);
             printf(" | b == %f", result.b.f_number);
             printf(" | c == %f\n\n", result.c.f_number);*/
 
-            if (check_is_quadratic(result.a.f_number, fp) == 1)
+            if (check_is_quadratic(result.a.f_number, log) == 1)
             {
                 d = discrim(result.a.f_number,
                             result.b.f_number,
@@ -91,7 +104,7 @@ int main(int argc, char const *argv[])
             printf("\tINVALID\n\n");
         }
     }
-    fclose(fp);
+    fclose(log);
     // printf("\n:::::: %s %f\n"," 3.4e4" ,strtof("3.4e4",NULL));
     new_line();
 
@@ -107,26 +120,26 @@ void init_var(nummber_parts *v)
     v->diff = 0;
 }
 
-int memory_ok(Line *linein, FILE *fp)
+int memory_ok(Line *linein, FILE *log)
 {
     if ((linein->str = malloc(NLINE)) == NULL)
     {
-        fprintf(fp, "Systems failure no memory for malloc(%d)\n", NLINE);
+        fprintf(log, "Systems failure no memory for malloc(%d)\n", NLINE);
         return 0;
     }
     linein->len = 0;
     linein->code = -1;
     linein->str = '\0';
-    fprintf(fp, "Systems is ok for memory of malloc(%d)\n", NLINE);
+    fprintf(log, "Systems is ok for memory of malloc(%d)\n", NLINE);
     return 1;
 }
 
-int solve_equation_yes_no(char *response, FILE *fp)
+int solve_equation_yes_no(char *response, FILE *log)
 {
     printf("Do you want to solve a quadratic eqauation Ax^2 + Bx + B = 0 ? (Y/N):");
     if (fgets(response, 2, stdin) == NULL)
     {
-        fprintf(fp, "Input error: can not read from stdin\n");
+        fprintf(log, "Input error: can not read from stdin\n");
         return -1;
     }
     if (strncmp(response, "Y", 3))
@@ -139,7 +152,20 @@ int solve_equation_yes_no(char *response, FILE *fp)
     }
     else
     {
-        fprintf(fp, "Input error: response must be a single character: Y or N\n");
+        fprintf(log, "Input error: response must be a single character: Y or N\n");
         return -1;
     }
+}
+
+int validator_abc(Line *linein, FILE *fp)
+{
+    printf("Input a b c :");
+    if (fgets(linein->str, linein->max, stdin) == NULL)
+    {
+        fprintf(fp, "Input error: can not read from stdin\n");
+        return -1;
+    }
+    
+    
+
 }
