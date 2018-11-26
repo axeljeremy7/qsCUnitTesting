8dea4b93fba742387d3fe5d38cb90f1eabf1fa62
axeljeremy7 <axel.jeremy.7@gmail.com>
2018-11-26 03:21:00 -0500

    handle better stdin func

--- src/main.c
+++ src/main.c
@@ -2,7 +2,7 @@
 
 int clean_stdin();
 int solve_equation_yes_no(FILE *log, char *response);
-int read_abc(FILE *log, char *a_array, char *b_array, char *c_array, char *response);
+int read_abc(FILE *log, char *a_array, char *b_array, char *c_array, char *response, char *line);
 int exit_main(FILE *log, char *response);
 int show_roots(float a, float b, float c, FILE *log, double *discriminant, int *num_roots, roots_numbers *result);
 double get_machine_epsilon();
@@ -22,6 +22,7 @@ int main(int argc, char const *argv[])
     double machine_epsilon = get_machine_epsilon();
     int again = 0;
     int yn = 1 ;
+     char *line = malloc(100);
     while (1)
     {
         if (again == 0){
@@ -30,7 +31,7 @@ int main(int argc, char const *argv[])
 
         if (yn == 1 || again == 1)
         {
-            if (read_abc(log, a_array, b_array, c_array, response) == 1)
+            if (read_abc(log, a_array, b_array, c_array, response, line) == 1)
             {
                 init_variables(&a, &b, &c, &result);
                 if (validation_floats(a_array, b_array, c_array, &a, &b, &c, log) == 1)
@@ -63,14 +64,15 @@ int main(int argc, char const *argv[])
         else
             break;
     }
-
+    free(line);
+    fclose(log);
     printf("\n");
     return 0;
 }
 
 int solve_equation_yes_no(FILE *log, char *response)
 {
-    printf("Do you want to solve a quadratic equation Ax^2 + Bx + B = 0 ? (Y/N):");
+    printf("Do you want to solve a quadratic equation Ax^2 + Bx + B = 0 ? (Y for yes/ Any letter for exit):");
     if (scanf("%1s", response) != EOF)
     {
         fprintf(log, "FILE: %s FUNC: %s LINE: %d: response[0] -> %c\n", __FILE__, __func__, __LINE__, response[0]);
@@ -79,28 +81,37 @@ int solve_equation_yes_no(FILE *log, char *response)
             fprintf(log, "FILE: %s FUNC: %s LINE: %d: Response is Y\n", __FILE__, __func__, __LINE__);
             return 1;
         }
-        else if (strncmp(response, "N", 1) == 0)
-        {
-            fprintf(log, "FILE: %s FUNC: %s LINE: %d: Response is N\n", __FILE__, __func__, __LINE__);
-            printf("EXITING\n");
-            return 0;
-        }
+        // else if (strncmp(response, "N", 1) == 0)
+        // {
+        //     fprintf(log, "FILE: %s FUNC: %s LINE: %d: Response is N\n", __FILE__, __func__, __LINE__);
+        //     printf("EXITING\n");
+        //     return 0;
+        // }
         else
         {
-            fprintf(log, "FILE: %s FUNC: %s LINE: %d: Input error: response must be a single character: Y or N\n", __FILE__, __func__, __LINE__);
-            printf("Input error: response must be a single character: Y or N\n");
+            // fprintf(log, "FILE: %s FUNC: %s LINE: %d: Input error: response must be a single character: Y or N\n", __FILE__, __func__, __LINE__);
+            // printf("Input error: response must be a single character: Y or N\n");
+            printf("EXITING\n");
             return -1;
         }
     }
     return 0;
 }
 
-int read_abc(FILE *log, char *a_array, char *b_array, char *c_array, char *response)
+int read_abc(FILE *log, char *a_array, char *b_array, char *c_array, char *response, char *line)
 {
     printf("Enter A B C: ");
-    char c;
+    char c ;
+    // int args = -1;
+    // char *line = malloc(100);
+    fgets(line, 100, stdin);
+    int args = sscanf(line,"%32s %32s %32s%c", a_array, b_array, c_array, &c);
+    // clean_stdin();
+    // fseek(stdin,SEEK_END,SEEK_END);
+    // printf("\n");
+    fprintf(log, "FILE: %s FUNC: %s LINE: %d: args -> %d\n", __FILE__, __func__, __LINE__, args);
     int r;
-    if ( scanf("%32s %32s %32s%c", a_array, b_array, c_array, &c) == 4 && c == '\n' && clean_stdin() )
+    if ( args == 4 && c == '\n' )//&& c == '\n'
     {
         fprintf(log, "FILE: %s FUNC: %s LINE: %d: A -> %s\n", __FILE__, __func__, __LINE__, a_array);
         fprintf(log, "FILE: %s FUNC: %s LINE: %d: B -> %s\n", __FILE__, __func__, __LINE__, b_array);
@@ -110,15 +121,23 @@ int read_abc(FILE *log, char *a_array, char *b_array, char *c_array, char *respo
     else
     {
         fprintf(log, "FILE: %s FUNC: %s LINE: %d: Input error: You input is more than 3 variables\n", __FILE__, __func__, __LINE__);
-        printf("Input error: You input is more than 3 variables\n");
+        printf("Input error: You input is more or less than 3 variables\n");
         r = 0;
     }
+    fprintf(log, "FILE: %s FUNC: %s LINE: %d: args -> %d  r -> %d c -> |%c|\n", __FILE__, __func__, __LINE__, args, r, c);
+    // free(line);
     return r;
 }
 
 int clean_stdin()
 {
     while (getchar()!='\n');
+    // int i = 0;
+    // while(i < 3)
+    // {
+    //     getchar();
+    //     i++;
+    // }
     return 1;
 }
 
